ab343f856ccd540577fab6b03bb4086b4f9163f4
Eadweard <804875728@qq.com>
2023-07-18 13:31:58 +0800

    createsvg

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d')!;
 const img = new Image();
 
+function createSvg(dom: HTMLElement, cloneDom: HTMLElement) {
+  const htmlsvg = `data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="${dom.offsetWidth}" height="${dom.offsetHeight}"><foreignObject x="0" y="0" width="100%" height="100%">${new XMLSerializer().serializeToString(cloneDom)}${document.querySelector('style')?.outerHTML}</foreignObject></svg>`
+}
+
 function html2img(dom: HTMLElement) {
   if (!dom) throw Error('dom is not defined')
   return new Promise((resolve, reject) => {
