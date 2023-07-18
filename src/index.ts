32a918ac112cc90edc47a6d373e97029b023ea6b
Eadweard <804875728@qq.com>
2023-07-18 13:17:50 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import { draw } from './core/canvas';
 
 const canvas = document.createElement('canvas');
-const context = canvas.getContext('2d');
+const context = canvas.getContext('2d')!;
+const img = new Image();
 
 function html2img(dom: HTMLElement) {
   if (!dom) throw Error('dom is not defined')
-  const cloneDom = dom.cloneNode(true) as HTMLElement;
-  cloneDom.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml'); // 设置一个xhtml命名空间
-  let imgDom: HTMLImageElement | NodeListOf<HTMLImageElement>;
-  if (cloneDom.tagName.toLowerCase() === 'img') {
-    imgDom = cloneDom as HTMLImageElement
-  } else {
-    imgDom = cloneDom.querySelectorAll('img')
-  }
-
+  return new Promise((resolve, reject) => {
+    let imgDom: NodeListOf<HTMLImageElement> | HTMLImageElement, imgList: HTMLImageElement[] = [];
+    const cloneDom = dom.cloneNode(true) as HTMLElement;
+    cloneDom.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml'); // 设置一个xhtml命名空间
+    if (cloneDom.tagName.toLowerCase() === 'img') {
+      imgDom = cloneDom as HTMLImageElement
+    } else {
+      imgDom = cloneDom.querySelectorAll('img')
+    }
+    if (imgDom) {
+      if (imgDom instanceof HTMLImageElement) {
+        imgList.push(imgDom)
+      } else {
+        imgList = Array.from(imgDom)
+      }
+    }
+    imgList.forEach((img, index) => {
+      draw(img, canvas, context)
+      img.src = canvas.toDataURL('image/png')
+    })
+  })
 }
 
 export default html2img;
