19cb83c7ed56b1f9265270c4d5ababc94dff0422
Eadweard <804875728@qq.com>
2023-07-18 11:38:39 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-function html2img(name: HTMLElement) {
-  console.log(name)
+function html2img(dom: HTMLElement) {
+  if (!dom) throw Error('dom is not defined')
+  const cloneDom = dom.cloneNode(true) as HTMLElement;
+  
 }
 
 export default html2img;
