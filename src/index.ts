550fe6028e7b490f2c23bf6c64670def7e121f26
Eadweard <804875728@qq.com>
2023-07-18 11:47:42 +0800

    一些逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 function html2img(dom: HTMLElement) {
   if (!dom) throw Error('dom is not defined')
   const cloneDom = dom.cloneNode(true) as HTMLElement;
-  
+  cloneDom.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml'); // 设置一个xhtml命名空间
+  let imgDom: HTMLImageElement | NodeListOf<HTMLImageElement>;
+  if (cloneDom.tagName.toLowerCase() === 'img') {
+    imgDom = cloneDom as HTMLImageElement
+  } else {
+    imgDom = cloneDom.querySelectorAll('img')
+  }
+
 }
 
 export default html2img;
